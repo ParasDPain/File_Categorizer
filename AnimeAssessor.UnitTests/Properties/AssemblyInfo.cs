e1903a5ad378841b99a2cedf96fa2f25bcb2a66f
Robert Snyder <immeraufdemhund@hotmail.com>
2016-01-20 18:56:26 -0600

    Added test project. Encapsulated repository class for getting anime tv titles.

---
+++ AnimeAssessor.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("AnimeAssessor.UnitTests")]
+[assembly: AssemblyProduct("AnimeAssessor.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AnimeAssessor/AnimeTvRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace AnimeAssessor
+{
+    public class AnimeTvRepository
+    {
+        public List<string> GetTvTitles(string filePath)
+        {
+            return LoadXML(filePath, "item", "name").OrderBy(x=> x).ToList();
+        }
+
+        private static IEnumerable<string> LoadXML(string filePath, string descendant, string element)
+        {
+            return XElement.Load(filePath)
+                    .Elements(descendant)
+                    .Where(c => c.Element("type").Value == "TV")
+                    .Select(c => DeAccentTitles(c.Element(element).Value));
+        }
+
+        private static string DeAccentTitles(string title)
+        {
+            char[] chars = title.Normalize(NormalizationForm.FormD)
+                 .Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+                 .ToArray();
+            return new string(chars).Normalize(NormalizationForm.FormC);
+        }
+    }
+}
